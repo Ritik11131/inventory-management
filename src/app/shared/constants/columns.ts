c9f065cc4f2a0fb5a845e62d2ea49064b34b23ae
Ritik11131 <ritik11131@gmail.com>
2024-11-18 20:25:31 +0530

    Added Online status

--- src/app/shared/constants/columns.ts
+++ src/app/shared/constants/columns.ts
@@ -16,6 +16,7 @@ export const deviceColumns = [
     { field: 'iccid', header: 'ICCID No', hyperlink: true},
     { field: 'vehicle', header: 'Vehicle No', nested:true, subfield:'vehicleNo', hyperlink: true},
     { field: 'user', header: 'Permit Holder',nested:true, subfield:'name', hyperlink: true },
+    { field: 'lastPosition', header: 'Online Status',nested:true, subfield:'status' },
     // { field: 'simDetails', header: 'Sim Provider',nested:true, subfield:'provider', nextsubfield:'providerName' },
     // { field: 'simDetails', header: 'Primary Sim Operator', nested:true, subfield:'primaryOpt' },
     // { field: 'simDetails', header: 'Primary Mobile No',nested:true, subfield:'primarySimNo' },
