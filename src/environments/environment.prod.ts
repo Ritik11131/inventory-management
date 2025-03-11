29cbebeae951cb6a7490400fa55d2d035de98142
Ritik11131 <ritik11131@gmail.com>
2025-03-11 14:11:14 +0530

    prod url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://vs.livetrack24.in/api',
+    apiUrl: 'https://grlprod.livetrack24.in/api',
     production: true,
     here: {
       apiKey: '3M9vTMCXbAq93ciboJSU13CSkZ4fJezXEcg9bCtlDNw'
