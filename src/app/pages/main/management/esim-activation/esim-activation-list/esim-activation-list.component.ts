8954e6e628ecb7ceba03e8c1a581dae2c143420a
Ritik11131 <ritik11131@gmail.com>
2025-03-22 22:14:10 +0530

    Removed unnecessary code

--- src/app/pages/main/management/esim-activation/esim-activation-list/esim-activation-list.component.ts
+++ src/app/pages/main/management/esim-activation/esim-activation-list/esim-activation-list.component.ts
@@ -81,38 +81,6 @@ export class EsimActivationListComponent {
   }
 
 
-  async fetchDevices(): Promise<any> {
-    this.isDevicesLoading = true;
-    this.devices = [];
-    try {
-      const { data } = await this.deviceService.getList(500, 1);
-      this.devices = data?.items || []; // Use optional chaining for safety
-
-      const { pageSize, totalPage } = data;
-
-      // Create an array of promises for fetching all pages
-      const pageRequests = Array.from({ length: totalPage - 1 }, (_, i) =>
-        this.deviceService.getList(pageSize, i + 2) // Start from page 2
-      );
-
-      // Wait for all requests to complete
-      const responses = await Promise.all(pageRequests);
-
-      // Concatenate all items from the responses
-      responses.forEach(response => {
-        this.devices = this.devices.concat(response.data?.items || []);
-      });
-
-      // Optional: Show success message
-      // this.toastService.showSuccess('Success', `${this.authService.getUser Type()} List fetched successfully!`);
-    } catch (error) {
-      this.toastService.showError('Error', `Failed to fetch Device List!`);
-    } finally {
-      this.isDevicesLoading = false;
-    }
-  }
-
-
   async fetchActivationTypeDevices(): Promise<any> {
     this.isDevicesLoading = true;
     this.devices = [];
